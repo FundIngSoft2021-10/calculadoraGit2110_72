11fc48e63fd274c6ff203cbe0a4d85a474e89628
Tatiana Pena <penap_at@javeriana.edu.co>
2021-02-23 22:19:51 -0500

    Quiz Angie Tatiana Peña

--- MyCalculator/src/mycalculator/CalculadoraATPP.java
+++ MyCalculator/src/mycalculator/CalculadoraATPP.java
@@ -5,28 +5,30 @@
 package mycalculator;
 
 public class CalculadoraATPP implements ICalculator{
+  @Override
   public String about() {
 
       return "Calculator implementend by: " + Authors.ATTP;
   }
 
-  public double suma (double a, double b){
+  @Override
+  public double add (double a, double b){
     return a+b;
   }
 
-  public double resta (double a, double b){
+  @Override
+  public double substract (double a, double b){
     return a-b;
   }
 
-  public double division (double a, double b){
+  @Override
+  public double divide (double a, double b){
     return a/b;
   }
 
-  public double multiplicación (double a, double b){
+  @Override
+  public double multiply (double a, double b){
     return a*b;
   }
 
-  public double potencia (double a, double b){
-    return pow(a,b);
-  }
 }
